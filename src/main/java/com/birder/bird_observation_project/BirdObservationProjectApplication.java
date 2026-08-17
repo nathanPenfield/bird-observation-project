17f2a158a6a28a86c03ff75a3532b5b00ee339f7
nathanPenfield <nathanpenfield3@gmail.com>
2026-08-17 16:20:17 -0400

    initial commit

---
+++ src/main/java/com/birder/bird_observation_project/BirdObservationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.birder.bird_observation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdObservationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdObservationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birder/bird_observation_project/BirdObservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birder.bird_observation_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdObservationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
