7a8c68fac9b66fe97e8d5a63117ed69993d201c8
nathanPenfield <nathanpenfield3@gmail.com>
2026-08-23 15:25:53 -0400

    temp fix of cors error by allowing localhost 5173

--- backend/src/main/java/com/birder/bird_observation_project/controllers/ObservationController.java
+++ backend/src/main/java/com/birder/bird_observation_project/controllers/ObservationController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/observations")
+@CrossOrigin(origins="http://localhost:5173")
 public class ObservationController {
     private ObservationService observationService;
     
