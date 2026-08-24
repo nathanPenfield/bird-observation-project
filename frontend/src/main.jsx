faad88058635187be40beda34ea0cf2d0baf81ae
nathanPenfield <nathanpenfield3@gmail.com>
2026-08-23 21:38:26 -0400

    init react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import MySightings from './pages/MySightings.jsx'
+import Home from './pages/Home.jsx'
 import "./index.css"
+
+const router = createBrowserRouter([
+  {'path':"/",'element':<Home/>},
+  {'path':"/mysightings",'element':<MySightings/>}
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MySightings/>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
