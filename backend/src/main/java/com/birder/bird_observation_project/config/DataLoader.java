a098c83a0f5818c9a6101472d46c1438b32a7a7c
nathanPenfield <nathanpenfield3@gmail.com>
2026-08-30 15:45:04 -0400

    load test data for locations

--- backend/src/main/java/com/birder/bird_observation_project/config/DataLoader.java
+++ backend/src/main/java/com/birder/bird_observation_project/config/DataLoader.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import com.birder.bird_observation_project.models.Location;
 import com.birder.bird_observation_project.models.Species;
+import com.birder.bird_observation_project.repositories.LocationRepository;
 import com.birder.bird_observation_project.repositories.SpeciesRepository;
 
 import tools.jackson.core.type.TypeReference;
@@ -28,6 +30,22 @@ public class DataLoader {
             try (InputStream input = new ClassPathResource("michigan-birds.json").getInputStream()){
                 List<Species> species = objectMapper.readValue(input, new TypeReference<List<Species>>() {});
                 repository.saveAll(species);
+            }
+        };
+    }
+
+    @Bean
+    CommandLineRunner loadLocations(LocationRepository repository, ObjectMapper objectMapper){
+        return args -> {
+            // if db has location data, skip 
+            if(repository.count() > 0){
+                return;
+            }
+            
+            // load seed location data into db
+            try (InputStream input = new ClassPathResource("test-locations.json").getInputStream()){
+                List<Location> locations = objectMapper.readValue(input, new TypeReference<List<Location>>() {});
+                repository.saveAll(locations);
             }
         };
     }
